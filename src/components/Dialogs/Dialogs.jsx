d7e178068ec9ba1c14937304647111ee6b4e1efc
Farid <farahardcore161@gmail.com>
2019-07-17 19:35:12 +0300

    filled Dialogs component and  styled him

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,24 @@ import s from "./Dialogs.module.css"
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+           <div className={s.dialogsItems}>
+               <div className={`${s.dialog} ${s.active}`}>
+                    User
+               </div>
+               <div className={`${s.dialog} ${s.active}`}>
+                    User
+               </div>
+               <div className={`${s.dialog} ${s.active}`}>
+                    User
+               </div>
+           </div>
+           <div className={s.mesages}>
+                <div className={s.message}>messages</div>
+                <div className={s.message}>messages</div>
+                <div className={s.message}>messages</div>
+           </div>
+
         </div>
     )
 }
