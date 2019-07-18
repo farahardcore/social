1bdfeaaa594f5e2e050e7b64a426172fdebb9076
Farid <farahardcore161@gmail.com>
2019-07-18 20:08:27 +0300

    add more components in Dialogs. delete pictures for app and add picture architecture of my app

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,17 +1,8 @@
 import React from "react"
 import s from "./Dialogs.module.css"
 import {NavLink} from "react-router-dom"
-
-const DialogItem = (props) => {
-    return (
-        <div className={`${s.dialog} ${s.active}`}>
-                    <NavLink to={`/dialogs/${props.id} `}>{props.name}</NavLink>
-               </div>
-    )
-}
-const Message = (props) => {
-    return <div className={s.message}>{props.message}</div>
-}
+import DialogItem from "./DialogItem/DialogItem";
+import Message from "./Message/Message";
 
 const Dialogs = (props) => {
     let dialogs = [
