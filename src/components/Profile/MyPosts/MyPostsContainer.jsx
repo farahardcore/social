7e7085b1472de749986c9692fec6ffe75d427b84
Farid <farahardcore161@gmail.com>
2020-02-03 22:25:02 +0300

    fixed connect react-redux

--- src/components/Profile/MyPosts/MyPostsContainer.jsx
+++ src/components/Profile/MyPosts/MyPostsContainer.jsx
@@ -5,7 +5,7 @@ import {connect} from "react-redux";
 
 
 
-let mapStateToProps= (state)=>{
+let mapStateToProps = (state)=>{
     return {
         posts : state.profilePage.posts,
         newPostText : state.profilePage.newPostText
--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {BrowserRouter} from "react-router-dom"
-import Provider from "react-redux/lib/components/Provider";
+;
+import {Provider} from "react-redux";
 
 
 
