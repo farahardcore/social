5c4d5bee9691f8eadf05e3d03c439adfcfcbec8b
Farid <farahardcore161@gmail.com>
2020-02-12 17:47:33 +0300

    classes-component and lifecycle of component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,29 +5,32 @@ import Friends from './Friends/Friends';
 import {friendsListActionCreator} from "../../Redux/sitebarReducer";
 
 const Navbar = (props) => {
-  let friendsElement = ()=> {
-     props.dispatch(friendsListActionCreator())
-  };
-  // let friendsList = props.state.friends.map(f => <Friends src={f.src}/>);
+    let friendsElement = () => {
+        props.dispatch(friendsListActionCreator())
+    };
+    // let friendsList = props.state.friends.map(f => <Friends src={f.src}/>);
 
-    return  <nav className={s.nav}>
-    <div className={s.item}>
-      <NavLink activeClassName={s.active} to="/profile">Profile</NavLink>
-    </div>
-    <div className={s.item}>
-      <NavLink activeClassName={s.active} to="/dialogs">Dialogs</NavLink>
-    </div>
-    <div className={s.item}>
-      <NavLink activeClassName={s.active} to="/news">News</NavLink>
-    </div>
-    <div className={s.item}>
-      <NavLink activeClassName={s.active} to="/music">Music</NavLink>
-    </div>
-    <div className={s.item}>
-      <NavLink activeClassName={s.active} to="/settings">Settings</NavLink>
-    </div>
-    {friendsElement}
-  </nav>
+    return <nav className={s.nav}>
+        <div className={s.item}>
+            <NavLink activeClassName={s.active} to="/profile">Profile</NavLink>
+        </div>
+        <div className={s.item}>
+            <NavLink activeClassName={s.active} to="/dialogs">Dialogs</NavLink>
+        </div>
+        <div className={s.item}>
+            <NavLink activeClassName={s.active} to="/users">Users</NavLink>
+        </div>
+        <div className={s.item}>
+            <NavLink activeClassName={s.active} to="/news">News</NavLink>
+        </div>
+        <div className={s.item}>
+            <NavLink activeClassName={s.active} to="/music">Music</NavLink>
+        </div>
+        <div className={s.item}>
+            <NavLink activeClassName={s.active} to="/settings">Settings</NavLink>
+        </div>
+        {friendsElement}
+    </nav>
 }
 
 export default Navbar;
--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,19 +1,23 @@
 import React from "react"
-import s from './users.module.css'
+import userPhoto from "../../assets/images/images.jfif";
+import s from "./users.module.css";
 import * as axios from "axios";
-import userPhoto from "./../../assets/images/images.jfif";
 
-let Users = (props) => {
-    let getUsers = () => {
-        if(props.users.length === 0){
-            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-                props.setUsers(response.data.items);
-            });
+class Users extends React.Component {
+    componentDidMount() {
+        alert("componentDidMount");
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+            this.props.setUsers(response.data.items);
+        });
     }
-    };
-    return <div>
-        <button onClick={getUsers}>Get USERS</button>
-            {props.users.map(user => <div key={user.id}>
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        alert("componentDidUpdate");
+    }
+
+
+    render() {
+       return <div>
+           {this.props.users.map(user => <div key={user.id}>
                 <span>
                     <div>
                         <img src={user.photos.small != null? user.photos.small : userPhoto } className={s.photo}/>
@@ -21,14 +25,14 @@ let Users = (props) => {
                     <div>
                         {user.followed
                             ? <button onClick={() => {
-                                props.unfollow(user.id)
+                                this.props.unfollow(user.id)
                             }}>Unfollow</button>
                             : <button onClick={() => {
-                                props.follow(user.id)
+                                this.props.follow(user.id)
                             }}>Follow</button>}
                     </div>
                 </span>
-                <span>
+               <span>
                     <span>
                         <div>{user.fullname}</div>
                         <div>{user.status}</div>
@@ -38,8 +42,9 @@ let Users = (props) => {
                         <div>{"user.location.country"}</div>
                     </span>
                 </span>
-            </div>)}
-        </div>
+           </div>)}
+       </div>
+   }
+}
 
-};
 export default Users;
