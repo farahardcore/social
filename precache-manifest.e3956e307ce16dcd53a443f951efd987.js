c6d19bb18974ecd5d6d0127b410287a27c84c9a5
Farid <farahardcore161@gmail.com>
2020-05-24 10:10:33 +0300

    Updates

---
+++ precache-manifest.e3956e307ce16dcd53a443f951efd987.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb93ea1749ed49834e7f08ff3d602286",
+    "url": "/social/index.html"
+  },
+  {
+    "revision": "44ae9b262310032c1514",
+    "url": "/social/static/css/3.e142a54e.chunk.css"
+  },
+  {
+    "revision": "4abd3d7a0f0d16bda2e0",
+    "url": "/social/static/css/main.e57d0874.chunk.css"
+  },
+  {
+    "revision": "46a6bfeae4e8a934b820",
+    "url": "/social/static/js/2.91b38ddd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/social/static/js/2.91b38ddd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44ae9b262310032c1514",
+    "url": "/social/static/js/3.dc1b1bb6.chunk.js"
+  },
+  {
+    "revision": "4abd3d7a0f0d16bda2e0",
+    "url": "/social/static/js/main.398f8c3e.chunk.js"
+  },
+  {
+    "revision": "afc5577aa5086aefce86",
+    "url": "/social/static/js/runtime-main.8ea762c9.js"
+  },
+  {
+    "revision": "cf5df36655faed78137ee7aca445d738",
+    "url": "/social/static/media/Pulse-1s-200px.cf5df366.svg"
+  },
+  {
+    "revision": "904467c3c292d2a01a603595538b6b35",
+    "url": "/social/static/media/images.904467c3.jfif"
+  }
+]);
